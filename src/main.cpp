f154f95d2573c736b7915538ce7adfb0249d73c8
Arvind Kasiliya <arvind@kasiliya.com>
2024-12-01 15:09:24 -0500

    Add block with wasd controls

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #include <print>
 
 int main() {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    auto window = sf::RenderWindow({1920u, 1080u}, "Game");
     window.setFramerateLimit(144);
+
+    auto block = sf::RectangleShape({25, 25});
+    block.setFillColor(sf::Color::White);
+    block.setPosition({(1920u - block.getSize().x) / 2, (1080 - block.getSize().y) / 2});
 
     while (window.isOpen()) {
         for (auto event = sf::Event(); window.pollEvent(event);) {
@@ -13,7 +17,23 @@ int main() {
             }
         }
 
+        if (window.hasFocus()) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                block.move(0, -5);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                block.move(-5, 0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                block.move(0, 5);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                block.move(5, 0);
+            }
+        }
+
         window.clear();
+        window.draw(block);
         window.display();
     }
 }
